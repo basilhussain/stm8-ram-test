ef4a60d350de1d886f904ef0b120cb63fb529835
Basil Hussain <basilhussain@users.noreply.github.com>
2023-01-04 15:02:34 +0000

    Add to test code toggle of GPIO upon reaching main()

--- test.c
+++ test.c
@@ -1,4 +1,12 @@
+#include <stdint.h>
 #include "ram_test.h"
+
+#define PC_ODR (*(volatile uint8_t *)(0x500A))
+#define PC_ODR_ODR5 5
+#define PC_DDR (*(volatile uint8_t *)(0x500C))
+#define PC_DDR_DDR5 5
+#define PC_CR1 (*(volatile uint8_t *)(0x500D))
+#define PC_CR1_C15 5
 
 unsigned char __sdcc_external_startup(void) {
 	// These are actually macros that jump directly to the test routine. When
@@ -11,6 +19,14 @@ unsigned char __sdcc_external_startup(void) {
 }
 
 void main(void) {
+	// Configure GPIO PC5 as an output.
+	PC_DDR = (1 << PC_DDR_DDR5);
+	PC_CR1 = (1 << PC_CR1_C15);
+	
+	// Turn GPIO PC5 on to signal main() has been successfully entered, e.g. by
+	// illuminating a connected LED, or similar.
+	PC_ODR |= (1 << PC_ODR_ODR5);
+	
 	while(1);
 }
 
